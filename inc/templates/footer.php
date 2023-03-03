9d9e478a5d95f82a40ba8052d5978ff76e4bda53
avillaq <villafuertequispealex@gmail.com>
2023-03-03 16:43:11 -0500

    Optimizando archivos php

---
+++ inc/templates/footer.php
@@ -0,0 +1,28 @@
+</main>
+<footer>
+        <div class="container-footer">
+            <div class="info-footer">
+                <div class="menu-footer">
+                    <p>Menu</p>
+                    <a href="index.php">Inicio</a>
+                    <a href="categorias.php">Categoria</a>
+                </div>
+                <div class="contacto-footer">
+                    <p>Contacto</p>
+                    <a href="mailto:villafuertequispealex@gmail.com">villafuertequispealex@gmail.com</a>
+                </div>
+                <div class="redes-footer">
+                    <a href="https://www.facebook.com/profile.php?id=100008164993137" ><i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://twitter.com/villafu00911721" ><i class="fa-brands fa-twitter"></i></a>
+                    <a href="https://github.com/avillaq" ><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+            <div class="copyright">
+                <p>&copy;2023 Tienda Anime - All Rights Reserved.</p>
+            </div>
+        </div>
+
+    </footer>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,36 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="css/main.css">        
-
-    <title>Tienda Anime</title>
-</head>
-<body>
-    <header>
-        <div class="side-header">
-            <div class="logo">
-                <img class="logo-img" src="img/logo.png" alt="Logo">
-                <a href="index.php" class="logo-title">TIENDA ANIME</a>
-            </div>
-
-            <div class="user-menu">
-                <a href="#"><i class="fa-regular fa-user"></i> Alex</a>
-                <a href="#"><i class="fa-solid fa-cart-shopping"></i> Carrito</a>
-                <a href="#"><i class="fa-solid fa-right-to-bracket"></i> Cerrar Sesion</a>
-            </div>
-        </div>
-    </header>
-    <main>
+<?php
+    require "inc/templates/header.php";
+?>
         <div class="servicios">
             <div class="gratis">
                 <i class="fa-solid fa-truck-moving"></i>
@@ -123,31 +93,8 @@
 
         </div>
 
-    </main>
-    <footer>
-        <div class="container-footer">
-            <div class="info-footer">
-                <div class="menu-footer">
-                    <p>Menu</p>
-                    <a href="index.php">Inicio</a>
-                    <a href="categoria.php">Categoria</a>
-                </div>
-                <div class="contacto-footer">
-                    <p>Contacto</p>
-                    <a href="mailto:villafuertequispealex@gmail.com">villafuertequispealex@gmail.com</a>
-                </div>
-                <div class="redes-footer">
-                    <a href="https://www.facebook.com/profile.php?id=100008164993137" ><i class="fa-brands fa-facebook"></i></a>
-                    <a href="https://twitter.com/villafu00911721" ><i class="fa-brands fa-twitter"></i></a>
-                    <a href="https://github.com/avillaq" ><i class="fa-brands fa-github"></i></a>
-                </div>
-            </div>
-            <div class="copyright">
-                <p>&copy;2023 Tienda Anime - All Rights Reserved.</p>
-            </div>
-        </div>
-
-    </footer>
     
-</body>
-</html>
+
+<?php
+    require "inc/templates/footer.php";
+?>
