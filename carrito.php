6f6e2eda7c9557240584b7371e51cf1126ff2d32
avillaq <villafuertequispealex@gmail.com>
2023-03-04 00:22:01 -0500

    Categorias terminado. Carrito terminado

---
+++ carrito.php
@@ -0,0 +1,55 @@
+<?php
+    require "inc/templates/header.php";
+?>
+    <div class="wrapper">
+        <h1>Carrito de Compras</h1>
+        <div class="container-carrito">
+            <div class="container-lista">
+                <div class="item-list">
+                    <img src="img/categorias/1187146-300x300.webp" alt="">
+                    <div class="item-detalle">
+                        <p>Camiseta de Anime de Ataque a los Titanes, Kyojin No Shingeki</p>
+                        <p>$50</p>
+                        <p><small>Cantidad:</small> 5</p>
+                        <a href="#">Eliminar</a>
+                    </div>
+                </div>
+                <div class="item-list">
+                    <img src="img/categorias/1187146-300x300.webp" alt="">
+                    <div class="item-detalle">
+                        <p>Camiseta de Anime de Ataque a los Titanes, Kyojin No Shingeki</p>
+                        <p>$50</p>
+                        <p><small>Cantidad:</small> 5</p>
+                        <a href="#">Eliminar</a>
+                    </div>
+                </div>
+                <div class="item-list">
+                    <img src="img/categorias/1187146-300x300.webp" alt="">
+                    <div class="item-detalle">
+                        <p>Camiseta de Anime de Ataque a los Titanes, Kyojin No Shingeki</p>
+                        <p>$50</p>
+                        <p><small>Cantidad:</small> 5</p>
+                        <a href="#">Eliminar</a>
+                    </div>
+                </div>
+                <div class="item-list">
+                    <img src="img/categorias/1187146-300x300.webp" alt="">
+                    <div class="item-detalle">
+                        <p>Camiseta de Anime de Ataque a los Titanes, Kyojin No Shingeki</p>
+                        <p>$50</p>
+                        <p><small>Cantidad:</small> 5</p>
+                        <a href="#">Eliminar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="container-resumen">
+                <h4>RESUMEN</h4>
+                <p>Total: $155</p>
+                <a href="#">Procesar Compra</a>
+            </div>
+        </div>
+    </div>
+
+<?php
+    require "inc/templates/footer.php";
+?>
