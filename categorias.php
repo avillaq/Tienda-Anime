ebb0daf34e67a5969cec85c09251b7a89b511557
avillaq <villafuertequispealex@gmail.com>
2023-03-03 22:24:14 -0500

    Imagenes para la categorias descargadas. Pagina categirias en proceso

---
+++ categorias.php
@@ -0,0 +1,48 @@
+<?php
+    require "inc/templates/header.php";
+?>
+    <div class="wrapper">
+        <h4>NUESTRAS CATEGORÍAS DE PRODUCTOS EN FUNCIÓN DEL ANIME</h4>
+        <div class="container-categorias">
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Shingeki No Kyojin</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+            <div class="card-categoria">
+                <img src="img/categorias/1187146-300x300.webp" alt="">
+                <a href="">Spy X Family</a>
+            </div>
+        </div>
+    </div>
+
+<?php
+    require "inc/templates/footer.php";
+?>
