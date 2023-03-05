a36deedcbdedafc9f6905dbc654a47c5e5815434
avillaq <villafuertequispealex@gmail.com>
2023-03-05 00:11:05 -0500

    Imagen de fondo listo

--- categoria-producto.php
+++ categoria-producto.php
@@ -1,16 +1,39 @@
 <?php
     require "inc/templates/header.php";
 ?>
-    <div class="bg-categoria-producto">
-        <div class="title-categoria-producto">
-            <p>Categoria</p>
-            <h1>Spy X Family</h1>
+
+    <div style="background-image:url('img/categorias/bg.png');">
+        <div class="bg-categoria-producto" style="">
+            <div class="title-categoria-producto">
+                <p>Categoria</p>
+                <h1>Spy X Family</h1>
+            </div>
         </div>
     </div>
 
     <div class="wrapper">
-        <h4>NUESTRAS CATEGORÍAS DE PRODUCTOS EN FUNCIÓN DEL ANIME</h4>
+        <div class="productos">
+                <div class="card">
+                    <img src="img/productos/Figuras-de-acci-n-de-Anime-de-ataque-a-los-Titanes-Levi-Ackerman-Mikasa-Ackerman-Eren.jpg_350x350.jpg" alt="">
+                    <div class="descripcion">
+                        <p><small>Figuras de Anime</small></p>
+                        <a href="detalles.php" class="nombre-producto">Figuras de acción de Anime de ataque a los Titanes</a>
+                        <p><small>$ 10</small></p>
+                    </div>
+                
+                </div>
+                <div class="card">
+                    <img src="img/productos/Figuras-de-acci-n-de-Anime-de-ataque-a-los-Titanes-Levi-Ackerman-Mikasa-Ackerman-Eren.jpg_350x350.jpg" alt="">
+                    <div class="descripcion">
+                        <p><small>Figuras de Anime</small></p>
+                        <a href="#" class="nombre-producto">Figuras de acción de Anime de ataque a los Titanes</a>
+                        <p><small>$ 10</small></p>
+                    </div>
+                
+                </div>
+        </div>
     </div>
+    
 
 <?php
     require "inc/templates/footer.php";
