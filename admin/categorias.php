48aee2e5668baaba6d876d861705f4c520052682
avillaq <villafuertequispealex@gmail.com>
2023-03-05 17:49:23 -0500

    creando tabla para la lista

--- admin/categorias.php
+++ admin/categorias.php
@@ -1,6 +1,68 @@
 <?php
 require "inc/templates/header.php";
 ?>
+    <div class="container">
+        <div class="contenedor-option">
+            <div class="contenedor-lista-crear">
+                <a href="">Lista de Categorias</a>
+                <a href="">Crear nueva Categoria</a>
+            </div>
+
+            <div class="contenedor-lista">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Imagen</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Figura de accion</td>
+                            <td><img src="../img/categorias/spy.webp" alt=""></td>
+                            <td>
+                                <a class="btn btn-editar" href="editar.php?id="><i class="fas fa-pen-square"></i></a>
+                                <button data-id="" type="button" class="btn btn-borrar"><i class="fas fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Figura de accion</td>
+                            <td><img src="../img/categorias/spy.webp" alt=""></td>
+                            <td>
+                                <a class="btn btn-editar" href="editar.php?id=">
+                                <i class="fas fa-pen-square"></i> 
+                                </a>
+                                <button data-id="" type="button" class="btn btn-borrar">
+                                <i class="fas fa-trash-alt"></i> 
+                                </button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Figura de accion</td>
+                            <td><img src="../img/categorias/spy.webp" alt=""></td>
+                            <td>
+                                <a class="btn btn-editar" href="editar.php?id=">
+                                <i class="fas fa-pen-square"></i> 
+                                </a>
+                                <button data-id="" type="button" class="btn btn-borrar">
+                                <i class="fas fa-trash-alt"></i> 
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
+
+            </div>
+
+            <!-- <div class="contenedor-crear">
+                HOlS
+            </div> -->
+        </div>
+    </div>
 
 
  <?php
--- admin/js/app.js
+++ admin/js/app.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded',init);
 function init(){
-    const nav = document.querySelector("#nav-menu");
-    nav.addEventListener("click",function(e){
-        if(e.target.classList.contains("btn-menu")){
-            let hermanos = getSiblings(e.target);
-            hermanos.forEach(i => {
-                i.classList.remove("active");
-            })
-            e.target.classList.add("active");
-        }
+    
 
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     function getSiblings(e) {
