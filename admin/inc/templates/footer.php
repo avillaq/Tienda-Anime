2c55deca7afe728e37b15f71beeec97cbea0c583
avillaq <villafuertequispealex@gmail.com>
2023-03-05 12:40:24 -0500

    Pagina de categorias en proceso...

---
+++ admin/inc/templates/footer.php
@@ -0,0 +1,10 @@
+</main>
+    <footer>
+        <div class="copyright">
+            <p>&copy;2023 Tienda Anime - All Rights Reserved.</p>
+        </div>
+    </footer>
+   
+    
+</body>
+</html>
---
+++ admin/inc/templates/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="css/main.css">        
+
+    <title>Tienda Anime</title>
+</head>
+<body>
+    <header>
+        <div class="side-header">
+            <div class="logo">
+                <img class="logo-img" src="../img/logo.png" alt="Logo">
+                <a href="index.php" class="logo-title">TIENDA ANIME - <small>Administracion</small></a>
+            </div>
+            <a href="#" class="btn-out"><i class="fa-solid fa-right-to-bracket"></i> Cerrar Sesion</a>
+        </div>
+
+        
+    </header>
+    <main>
+        <div class="navegacion">
+            <h2><i class="fa fa-user"></i> Admin</h2>
+            <a href="index.php">Dashboard</a>
+            <a href="usuarios.php">Usuarios</a>
+            <a href="productos.php">Productos</a>
+            <a href="categorias.php">Categorias</a>
+        </div>
