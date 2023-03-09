bf370278b2c3905535af2b8b02e604fa4f1fb9be
avillaq <villafuertequispealex@gmail.com>
2023-03-09 12:30:03 -0500

    se agragaron productos hasta la categoria ONE PIECE

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,19 @@
 <?php
 require "inc/templates/header.php";
 ?>
+    <div class="container">
+        <div class="contenedor-option">
+
+            <h2>Dashboard de administracion</h2>
+
+            <p>Total usuario</p>
+            <p>Total productos</p>
+            <p>Total categorias</p>
+        
+            <p>Total ganancias</p>
+            <p>Total compras realizadas</p>
+        </div>
+    </div>
 
 
  <?php
