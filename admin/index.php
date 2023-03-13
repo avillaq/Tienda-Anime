41c99270a3a6b25b3d8d11e4a457e2e66cd98c4b
avillaq <villafuertequispealex@gmail.com>
2023-03-12 19:11:06 -0500

    tarjetas de informacion en dashboard (tarjeta base terminado)

--- admin/index.php
+++ admin/index.php
@@ -5,15 +5,32 @@ require "inc/templates/header.php";
 ?>
     <div class="container">
         <div class="contenedor-option">
-
             <h2>Dashboard de administracion</h2>
 
-            <p>Total usuario</p>
+            <div class="contenedor-dashboard">
+                <div class="card-info">
+                    <div class="text-icon">
+                        <div class="text-info">
+                            <p class="cantidad">150</p>
+                            <p>Total usuarios</p>
+                        </div>
+                        <i class="fa-solid fa-bag-shopping"></i>
+                    </div>
+                    <a href="#">Mas informacion <i class="fa-solid fa-circle-arrow-right"></i></a>
+                </div>
+
+
+
+            </div>
+
+            
+
+            <!-- <p>Total usuarios</p>
             <p>Total productos</p>
             <p>Total categorias</p>
+            <p>Total pagados</p>
         
-            <p>Total ganancias</p>
-            <p>Total compras realizadas</p>
+            <p>Ganancias totales </p>-->
         </div>
     </div>
 
