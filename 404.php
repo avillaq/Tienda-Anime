163fc8b116dca94496d584b91177c66b222dbe39
avillaq <villafuertequispealex@gmail.com>
2023-03-14 00:22:13 -0500

    archivo .htaccess creado

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+    require "inc/templates/header.php";
+?>
+    <div class="wrapper">
+
+        
+    </div>
+
+<?php
+    require "inc/templates/footer.php";
+?>
--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -29,6 +29,5 @@
     <script src="js/carrito-fetch.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
 
-    
 </body>
 </html>
