8d6c8e87d4d6d443f97e458bf283a00c7d38b7e2
avillaq <villafuertequispealex@gmail.com>
2024-04-26 10:55:52 -0500

    Update database connection credentials in conexionbd.php

--- inc/funciones/conexionbd.php
+++ inc/funciones/conexionbd.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost","root","","tiendaanime");
+    $conn = new mysqli("localhost","root","","animetienda");
     if ($conn->connect_error) {
         echo $error -> $conn->connect_error;
     }
